91d287e008ad427339f8b68b40fd03b190841f52
Pat <patrick.prendergast@dsto.defence.gov.au>
2017-03-24 10:28:23 +1030

    Tested City.equals in case it was the issue with edge recomb.

--- src/ec2017/ga/tsp/test/CityTest.java
+++ src/ec2017/ga/tsp/test/CityTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ec2017.ga.general.Symbol;
 import ec2017.ga.tsp.City;
 import ec2017.ga.tsp.SymCity;
 
@@ -24,6 +25,17 @@ public class CityTest
 		assertTrue(symB.distanceTo(symA) == 5.0);
 		assertTrue(symA.distanceTo(symB) == 5.0);
 		assertTrue(symB.distanceTo(symA) == 5.0);
+	}
+	
+	@Test
+	public void testEquals()
+	{
+		Symbol cityA = new City("A", 10, 11);
+		Symbol cityB = new City("B", 13, 14);
+		Symbol cityA2 = new City("A", 10, 11);
+		
+		assertFalse(cityA.equals(cityB));
+		assertTrue(cityA.equals(cityA2));
 	}
 
 }
