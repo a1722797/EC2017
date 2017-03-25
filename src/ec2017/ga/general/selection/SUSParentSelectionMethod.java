09116ecf153f825569015341fd281367bc2af198
Fergus Dall <fergus@beware.dropbear.id.au>
2017-03-25 19:36:02 +1030

    Calculate the spacing for an n-armed roulette wheel correctly

--- src/ec2017/ga/general/selection/SUSParentSelectionMethod.java
+++ src/ec2017/ga/general/selection/SUSParentSelectionMethod.java
@@ -6,6 +6,8 @@ import ec2017.ga.general.ParentSelectionMethod;
 import java.util.ArrayList;
 
 /**
+ * Implementation of n-armed roulette wheel fitness proportinal selection
+ *
  * Created by yanshuo on 17/3/13.
  */
 public class SUSParentSelectionMethod implements ParentSelectionMethod {
@@ -13,12 +15,14 @@ public class SUSParentSelectionMethod implements ParentSelectionMethod {
     public ArrayList<Individual> select(ArrayList<Individual> population) {
         ArrayList<Individual> selectedParents = new ArrayList<Individual>();
         double sumOfFitness = 0;
-        double space = sumOfFitness / population.size();
-        double starter = Math.random() * space;
 
         for(int i = 0; i < population.size(); i++){
             sumOfFitness += population.get(i).getFitness();
         }
+
+        double space = sumOfFitness / population.size();
+        double starter = Math.random() * space;
+
         for(int i = 0; i < population.size(); i++){
             int index = 0;
             double curSumOfFitness = population.get(0).getFitness();
