bfd6e9ee4fa9659212fd1b6b451f67ee3ededc55
Fergus Dall <fergus@beware.dropbear.id.au>
2017-03-25 19:41:16 +1030

    Run tournament selection by some simple shuffling and sorting

--- src/ec2017/ga/general/selection/TournamentParentSelectionMethod.java
+++ src/ec2017/ga/general/selection/TournamentParentSelectionMethod.java
@@ -4,6 +4,8 @@ import ec2017.ga.general.Individual;
 import ec2017.ga.general.ParentSelectionMethod;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by yanshuo on 17/3/14.
@@ -22,49 +24,24 @@ public class TournamentParentSelectionMethod implements ParentSelectionMethod {
 
     @Override
     public ArrayList<Individual> select(ArrayList<Individual> population) {
-        ArrayList<Individual> selectedParents = new ArrayList<Individual>();
+        ArrayList<Individual> selectedParents = new ArrayList<Individual>(population.size());
         for(int i = 0; i < population.size(); i++) {
-            int Index = getBestKIndividuals(population);
-            selectedParents.add(population.get(Index));
+            selectedParents.add(getBestKIndividuals(population));
         }
         return selectedParents;
     }
 
-    private int getBestKIndividuals(ArrayList<Individual> population){
-        //ArrayList<Individual> outcome = new ArrayList<Individual>();
-        int[] indexSet = new int[_k];
-        double[] fitness = new double[_k];
-        for(int i = 0; i < _k; i++){
-            int randomNum = (int)( Math.random()*population.size() );
-            indexSet[i] = randomNum;
-            fitness[i] = population.get(randomNum).getFitness();
-        }
+    private Individual getBestKIndividuals(ArrayList<Individual> population){
+    	// Pick _k individuals at random
+    	Collections.shuffle(population);
+    	List<Individual> kSubList = population.subList(0, _k);
 
+    	// Sort by fitness
+    	Collections.sort(kSubList);
+    	Collections.reverse(kSubList);
 
-        for(int i = 0; i < _k; i++){
-            for(int j = 0; j <_k; j++){
-                if(fitness[i] < fitness[j]){
-                    double tmp = 0;
-                    tmp = fitness[j];
-                    fitness[j] = fitness[i];
-                    fitness[i] = tmp;
-
-                    int tmpIndex = -1;
-                    tmpIndex = indexSet[j];
-                    indexSet[j] = indexSet[i];
-                    indexSet[i] = tmpIndex;
-                }
-            }
-        }
-
-        double proNum = _pro;
-        for(int i = 0; i < _k; i++){
-            if(Math.random() < proNum){
-                return indexSet[i];
-            }
-            proNum *= _pro;
-        }
-        return indexSet[0];
+    	// Pick best individual
+    	return kSubList.get(0);
     }
 
 
