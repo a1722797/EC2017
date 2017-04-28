f7aa4152ac86e41a5797c308e063eef6f7ff5a9b
YanShuo1992 <shuo.yan1992@gmail.com>
2017-04-29 00:02:49 +0930

    a little change for ex2, no influence on others

--- src/Driver.java
+++ src/Driver.java
@@ -21,7 +21,7 @@ public class Driver {
     /* The current sequence of parameters is
      * args[0]  folder with TTP files
      * args[1]  pattern to identify the TTP problems that should be solved
-     * args[2]  optimisation approach chosen
+     * args[2]  optimisation approach chosen 1,2,12,22
      * args[3]  stopping criterion: number of evaluations without improvement
      * args[4]  stopping criterion: time in milliseconds (e.g., 60000 equals 1 minute)
      */
@@ -29,10 +29,10 @@ public class Driver {
        
         if (args.length==0) 
 //            args = new String[]{"instances", "a280_n1395_bounded-strongly-corr_", // to do all 10 instances (several files match the pattern)
-//            args = new String[]{"instances", "a280_n1395_bounded-strongly-corr_10.ttp", // to do just this 1 instance
-            args = new String[]{"instances", "fnl4461_n4460_bounded-strongly-corr_01.ttp", // to do just this 1 instance
+            args = new String[]{"instances", "a280_n1395_bounded-strongly-corr_10.ttp", // to do just this 1 instance
+//            args = new String[]{"instances", "fnl4461_n4460_bounded-strongly-corr_01.ttp", // to do just this 1 instance
 //            args = new String[]{"instances", "pla33810_n338090_uncorr_10.ttp", // to do just this 1 instance
-            "2", "10000", "60000"};
+            "12", "10000", "60000"};
 //        ttp.Optimisation.Optimisation.doAllLinkernTours();
 //        runSomeTests();
         doBatch(args);
@@ -54,12 +54,13 @@ public class Driver {
         for (File f:files) {
             // read the TSP instance
             TTPInstance instance = new TTPInstance(f);
-            
+            //System.out.println(instance.items.length);
             long startTime = System.currentTimeMillis();
             String resultTitle = instance.file.getName() + ".NameOfTheAlgorithm." + startTime;
             
             // generate a Linkern tour (or read it if it already exists)
             int[] tour = Optimisation.linkernTour(instance);
+
 
             System.out.print(f.getName()+": ");
             
